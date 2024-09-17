d29fc98954b50a5377565a47c7c682294a7ec482
Richard Miguel Irala <richardirala47@hotmail.com>
2024-09-17 17:24:20 -0300

    Delete unused vars

--- src/utils/primarySells/constants.ts
+++ src/utils/primarySells/constants.ts
@@ -1,34 +1,8 @@
 import { Address } from "viem";
 
-// sales params
-export const NFT_TOKEN_ADDRESS_AMOY =
-  "0x888a322db4b8033bac3ff84412738c096f87f9d0";
-export const SALES_CONTRACT_ADDRESS_AMOY =
-  "0x0327b2f274e04d292e74a06809bcd687c63a4ba4";
-export const CHAIN_ID_AMOY = 80002;
-export const SALES_CONTRACT_ADDRESS_ARBITRUM_SEPOLIA =
-  "0x326d2fbe4808dd2a3e205aecc5e25a6322ad0a81";
-export const NFT_TOKEN_ADDRESS_ARBITRUM_SEPOLIA =
-  "0xd4bb59d0ba1f7b2beea4c6d9b9f151ee1da02665";
-export const CHAIN_ID_ARBITRUM_SEPOLIA = 421614;
 export interface SaleItem {
   tokenId: string;
 }
-
-export const itemsForSalesAmoy: SaleItem[] = [
-  {
-    tokenId: "0",
-  },
-  {
-    tokenId: "1",
-  },
-];
-
-export const itemsForSalesArbitrumSepolia: SaleItem[] = [
-  {
-    tokenId: "0",
-  },
-];
 
 export interface SaleConfigurationProps {
   networkName: string;
