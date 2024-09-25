97905bf4c551cbc221c36e0d50f04e4139a32063
Tomasz Dysinski <dysinski.tomasz@gmail.com>
2024-09-25 16:10:14 -0700

    fixed chain finder

--- src/utils/chains.tsx
+++ src/utils/chains.tsx
@@ -2,7 +2,15 @@ import type { Chain } from "wagmi/chains";
 import { friendlySalesConfigurations } from "./primarySales/constants";
 
 const chains = Array.from(
-  new Set(friendlySalesConfigurations.map((item) => item.chain)),
+  new Set(
+    friendlySalesConfigurations.map((item) => {
+      const chain = chains.find((chain) => chain.id === item.chainId);
+      if (!chain) {
+        throw new Error(`No chain with id ${item.chainId}`);
+      }
+      return chain;
+    }),
+  ),
 ) as [Chain, ...Chain[]];
 
 export default chains;
