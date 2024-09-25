c3b66657783ed941893ca90daf630aeba5563b5f
Tomasz Dysinski <dysinski.tomasz@gmail.com>
2024-09-25 15:36:49 -0700

    deduce chains from friendly settings

--- src/utils/chains.tsx
+++ src/utils/chains.tsx
@@ -1,6 +1,8 @@
-import { polygonAmoy, arbitrumSepolia } from "wagmi/chains";
 import type { Chain } from "wagmi/chains";
+import { friendlySalesConfigurations } from "./primarySales/constants";
 
-const chains = [polygonAmoy, arbitrumSepolia] as [Chain, ...Chain[]];
+const chains = Array.from(
+  new Set(friendlySalesConfigurations.map((item) => item.chain)),
+) as [Chain, ...Chain[]];
 
 export default chains;
