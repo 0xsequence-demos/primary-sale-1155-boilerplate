36d4ab5e3fc7f5bcfc3a0824dd307acde37daa26
Matt Rintoul <matt@sayyeah.com>
2025-01-22 16:18:12 -0500

    Fix github repo link

--- src/App.tsx
+++ src/App.tsx
@@ -86,7 +86,7 @@ function App() {
 
   return (
     <SequenceBoilerplate
-      githubUrl="https://github.com/0xsequence-demos/kit-embedded-wallet-remix-cloudflare-boilerplate"
+      githubUrl="https://github.com/0xsequence-demos/primary-sale-1155-boilerplate/"
       name="Primary Sale 1155 Boilerplate"
       description="Example of how to perform primary sales of 1155 NFTs using Sequence."
       docsUrl="https://docs.sequence.xyz/"
