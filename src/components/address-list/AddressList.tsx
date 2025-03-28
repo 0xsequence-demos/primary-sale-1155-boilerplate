f5696d0ecca8a3943940a61b5f4a44efb4c7575d
Tomasz Dysinski <dysinski.tomasz@gmail.com>
2025-03-27 17:10:39 -0700

    simplified and fixed address list

--- src/components/address-list/AddressList.tsx
+++ src/components/address-list/AddressList.tsx
@@ -1,27 +1,13 @@
-import { Children, cloneElement, type ReactElement } from "react";
-import { getChain } from "~/config/ERC20/getChain";
+import { type ReactElement } from "react";
 
 export function AddressList({
   children,
-  chainId,
 }: {
   children: ReactElement | ReactElement[];
-  chainId: number;
 }) {
-  const url = `${getChain(chainId)?.blockExplorer?.rootUrl}/address/`;
-
-  const childrenWithProps = Children.map(
-    children as ReactElement | ReactElement[],
-    (child: ReactElement) =>
-      cloneElement(child, {
-        url,
-        ...child.props,
-      }),
-  );
-
   return (
     <ul className="flex flex-col gap-4 text-14 text-left leading-tight">
-      {childrenWithProps}
+      {children}
     </ul>
   );
 }
